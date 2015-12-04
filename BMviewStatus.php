8cc8ef3609c164c75d029a5117f9c341eaa043a3
raiyan2723 <raiyanshaikh1993@gmail.com>
2015-12-04 12:40:35 +0530

    Added from and to page

--- BMviewStatus.php
+++ BMviewStatus.php
@@ -8,7 +8,9 @@ if (!isset($_SESSION['bdmemp'])) {
 }
 require_once('header.php');
 ?>
+<script src="jquery-ui.js" type="text/javascript"></script>
 <div class="row">
+    
     <div class="col-lg-12">
         <h3 class="page-header">View Report</h3>
     </div>
@@ -19,11 +21,11 @@ require_once('header.php');
         <form>
             <div class="form-group">
                 <label class="control-label">From</label>
-                <input type="date" class="form-control" name="from" placeholder="From"/>
+                <input type="date" class="form-control from" name="from" placeholder="From"/>
             </div>
             <div class="form-group">
                 <label class="control-label">To</label>
-                <input type="date" class="form-control" name="from" placeholder="From"/>
+                <input type="date" class="form-control to" name="to" placeholder="To"/>
             </div>
             <div class="form-group">
                 <input type="submit" class="btn btn-success" value="Get Report"/>
@@ -32,6 +34,14 @@ require_once('header.php');
         </div>
     </div>
 </div>
+<script>
+    var dateToday = new Date();
+    var dates = $(".from, .to").datepicker({
+        defaultDate: "+1w",
+        changeMonth: true,
+        numberOfMonths: 1,
+    });
+</script>
 <?php 
 require_once ('footer.php');
 ?>
