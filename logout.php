4d214e65d3591305ab39b384a6f6b3c8db0cf8c7
raiyan2723 <raiyanshaikh1993@gmail.com>
2015-12-04 12:08:02 +0530

    chnages in logout

--- logout.php
+++ logout.php
@@ -1,9 +1,7 @@
 <?php
 session_start();
 session_destroy();
-    $_SESSION['bdm'] = null;
-      $_SESSION['ho'] = null;
-        $_SESSION['taskforce'] = null;
+    
     header("Location:index.php");
 
 echo '<script>window.location="index.php"</script>';
