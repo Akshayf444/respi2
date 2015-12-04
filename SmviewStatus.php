6ef052dcf2c336b2d026feb7ef85e74cee43ffd2
Akshayf444 <mohite.akshay118@gmail.com>
2015-12-04 15:32:47 +0530

    Added Dynamic BM list

--- SmviewStatus.php
+++ SmviewStatus.php
@@ -1,5 +1,4 @@
 <?php
-
 require_once("./includes/initialize.php");
 
 session_start();
@@ -8,6 +7,10 @@ if (!isset($_SESSION['smemp'])) {
     exit();
 }
 $sm_empid = $_SESSION['smemp'];
+
+$conditions = array("WHERE act.SM_EMP_ID = '$sm_empid' GROUP BY rm.BM_EMP_ID ");
+$topper = man_power::bmViewStatus($conditions);
+
 require_once('header.php');
 ?>
 <script src="jquery-ui.js" type="text/javascript"></script>
@@ -16,26 +19,33 @@ require_once('header.php');
 <div class="row">
     <div class="col-lg-12">
         <div class="col-xs-12">
-    <table class="table table-bordered table-stripped">
-        <tr style="background: #5BC6DE">
-            <th>BM Name</th>
-            <th>Dr Started Practicing Change</th>
-            <th>Rotahaler Check points</th>
-            <th>RCP Drives</th>
-            <th>Used Rotahaler Changed</th>
-        </tr>
-        
-
-            
-
-        
-    </table>
-</div>
+            <table class="table table-bordered table-stripped">
+                <tr style="background: #5BC6DE">
+                    <th>BM Name</th>
+                    <th>Dr Started Practicing Change</th>
+                    <th>Rotahaler Check points</th>
+                    <th>RCP Drives</th>
+                    <th>Used Rotahaler Changed</th>
+                </tr>
+                <?php
+                if (!empty($topper)) {
+                    foreach ($topper as $value) {
+                        echo '<tr>'
+                        . '<td>' . $value->BM_Name . '</td>'
+                        . '<td>' . $value->Practicing_Change . '</td>'
+                        . '<td>' . $value->Check_Points . '</td>'
+                        . '<td>' . $value->RCP_Drives . '</td>'
+                        . '<td>' . $value->Rotahaler . '</td>'
+                        . '</tr>';
+                    }
+                }
+                ?>
+            </table>
+        </div>
 
     </div>
 </div>
 
 <?php
-
 require_once ('footer.php');
 ?>
